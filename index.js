60febf064eeef0f69f1f666707565f9a4b70ef40
Bruno Vinicius <brunooliver368@gmail.com>
2022-06-03 17:43:01 -0300

    final v

--- index.js
+++ index.js
@@ -4,37 +4,50 @@ const app = express()
 const users = []
 app.use(express.json())
 
+const firstMiddleware = (req, res, next) => {
+    const { id } = req.params
+    const i = users.findIndex(user => user.id === id)
+    if(i < 0){
+        return res.status(404).json({ error: "User Not Found" })
+    }
+    req.userIndex = i
+    req.userId = id
+    next()
+}
+
 // verificar usuário
 app.get('/user', (req, res) => {
     return res.status(201).json(users)
 })
 // criar usuário
 app.post('/user', (req, res) => {
-    const {name, age} = req.body
-    const user = {id: uuid.v4(), name, age}
-    users.push(user) 
-    return  res.status(201).json(users)
+    const { name, age} = req.body
+    const newUser = {id: uuid.v4(), name, age}
+    users.push(newUser)
+    return res.status(201).json(newUser)
 })
 // atualizar usuário
-app.put('/user/:id', (req, res) => {
-    const { id } = req.params
+app.put('/user/:id', firstMiddleware, (req, res) => {
     const { name, age } = req.body
-    const updateUsers = { id, name, age }
-    const i = users.findIndex(user => user.id === id)
-    if(i < 0) {
-        return res.status(404).json({message: "User Not Found"})
-    }
-    users[i] = updateUsers
-    return res.json(updateUsers)
+    const i = req.userIndex
+    const id = req.userId
+    const upUsers = { id, name, age}
+    users[i] = upUsers
+    return res.json(upUsers)
 })
 // deletar usuário
-app.delete('/user/:id' ,(req, res) => {
-    const { id } = req.params
-    const i = users.findIndex(user => user.id === id)
-    if(i < 0){
-        return res.status(404).json({message : "User Not Found"})
-    }
-    users.splice( i, 1 )
+app.delete('/user/:id', firstMiddleware, (req, res) => {
+    const { name, age } = req.body
+    const i = req.userIndex
+    const id = req.userId
+    users.splice(i , 1)
     return res.status(204).json()
 })
 app.listen(3003)
+
+
+
+
+
+
+
